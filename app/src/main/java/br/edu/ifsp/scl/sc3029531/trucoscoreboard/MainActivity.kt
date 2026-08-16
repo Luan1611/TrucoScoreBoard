c3c95f6f4372891d4eafe6d6fcfae8121c127799
Luan1611 <luan_1611@hotmail.com>
2026-08-16 12:31:14 -0300

    Basic score logic done

--- app/src/main/java/br/edu/ifsp/scl/sc3029531/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3029531/trucoscoreboard/MainActivity.kt
@@ -5,16 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.sc3029531.trucoscoreboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var scoreTeamA: Int = 0
+    private var scoreTeamB: Int = 0
+
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(activityMainBinding.root)
+
+        with(activityMainBinding) {
+            equipeASomar1.setOnClickListener {
+                scoreTeamA++
+                equipeAScore.apply{text = scoreTeamA.toString()}
+            }
+            equipeBSomar1.setOnClickListener {
+                scoreTeamB++
+                equipeBScore.apply{text = scoreTeamB.toString()}
+            }
+            equipeASomar3.setOnClickListener {
+                scoreTeamA += 3
+                equipeAScore.apply{text = scoreTeamA.toString()}
+            }
+            equipeBSomar3.setOnClickListener {
+                scoreTeamB += 3
+                equipeBScore.apply{text = scoreTeamB.toString()}
+            }
+
+            resetGame.setOnClickListener {
+                scoreTeamA = 0
+                scoreTeamB = 0
+                equipeAScore.apply { text = scoreTeamA.toString() }
+                equipeBScore.apply { text = scoreTeamB.toString() }
+            }
         }
+
     }
 }
